d8de43cd2bd0fc676bc95cf0d015919c79b98507
NoSa1e <cjsp1217@gmail.com>
2026-01-15 10:42:25 +0900

    push

--- src/test/java/com/ohgiraffers/jenkins/service/CalculatorServiceTest.java
+++ src/test/java/com/ohgiraffers/jenkins/service/CalculatorServiceTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CalculatorServiceTest {
 
     @Autowired
